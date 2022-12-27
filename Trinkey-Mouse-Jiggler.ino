465d1bbe95ed0eb50472f4380d625041bb93e6f3
Saif Sabban <66663433+saifsabban@users.noreply.github.com>
2022-12-27 00:57:22 +0000

    Update Trinkey-Mouse-Jiggler.ino

--- Trinkey-Mouse-Jiggler.ino
+++ Trinkey-Mouse-Jiggler.ino
@@ -6,7 +6,7 @@
 
 void setup() { // The Set-up loop. It runs once at the start of the code
   delay(100); // Gives a delay at the start of the code to help with ramdom seed
-  randomSeed(analogRead(0)); // Reads the value from an unused analog pin to seed the sudo random funtion
+  randomSeed(analogRead(0)); // Reads the value from an unused analog pin to seed the sudo random function
   Mouse.begin(); // Function that starts the mouse emulation
 }
 
@@ -15,7 +15,7 @@ void loop() { // The main loop of you code that will run indefinitely
 }
 
 void MouseJiggler(unsigned int Function_Interval) { // The mouse jiggler function, uses and input for the mean time interval
-  static unsigned long interval_Timer; // The is an unsigend long that will remember the last time the if statment was run
+  static unsigned long interval_Timer; // The is an unsigned long that will remember the last time the if statement was run
   static int Jiggle_interval; // The is an in that will keep the randomised Jiggle_interval in the function
 
   if ((millis() - interval_Timer) >= Jiggle_interval) { // The main if statement that check if the interval between last running the function, and the current time, has excceded the jiggle interval
@@ -27,6 +27,6 @@ void MouseJiggler(unsigned int Function_Interval) { // The mouse jiggler functio
       delay(3); // A 3 milli second delay to give time for the computer to react
     }
     Jiggle_interval = random(Function_Interval - (Function_Interval / 2), Function_Interval + (Function_Interval / 2)); // Sets a new random Jiggle_interval that is +- 50% of the Function_Interval
-    interval_Timer = millis(); // Sets the interval_Timer to th current time at the end of the if statment
+    interval_Timer = millis(); // Sets the interval_Timer to th current time at the end of the if statement
   }
 }
